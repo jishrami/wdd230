c4953aa2c65fa3d8d23e8b1c193106ddb0d11c7f
jishrami <jramirez@byui.edu>
2024-03-14 11:33:41 -0600

    called json, finished directory

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,59 @@
+const url = "./data/members.json";
+
+async function getDirectory() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    displayDirectory(data);
+}
+
+function displayDirectory(members) {
+    const main = document.querySelector("main");
+    main.setAttribute("class", "directory");
+
+    const buttons = `
+    <div id="directory-buttons">
+        <button id="listView">List</button>
+        <button id="gridView">Grid</button>
+    </div>
+    `;
+
+    main.innerHTML = buttons;
+
+    const board = document.createElement("article");
+    board.setAttribute("class", "board gridView"); // Initial view
+    main.appendChild(board);
+
+    const companies = members.companies;
+    for (const member of companies) {
+        const section = document.createElement("section");
+        section.setAttribute("class", "member-section");
+
+        const card = `
+        <div class="member-card">
+            <h2>${member.name}</h2>
+            <img src="${member.image_url}" alt="${member.name}" class="member-logo">
+            <p>${member.phone}</p>
+            <a href="${member.website}" target="_blank">${member.website}</a>
+            <p>${member.other_info}</p>
+        </div>
+        `;
+        section.innerHTML = card;
+        board.appendChild(section);
+    }
+
+    const listButton = document.querySelector("#listView");
+    const gridButton = document.querySelector("#gridView");
+
+    listButton.addEventListener("click", () => {
+        board.classList.remove("gridView");
+        board.classList.add("listView");
+    });
+
+    gridButton.addEventListener("click", () => {
+        board.classList.remove("listView");
+        board.classList.add("gridView");
+    });
+}
+
+getDirectory();
--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,17 @@
 const baseURL = "https://jishrami.github.io/wdd230/";
 const linksURL = "https://jishrami.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data); // this is for testing
+    displayLinks(data);
+}
+
+function displayLinks(weeks) {
+    weeks.forEach(week => {
+        
+    });
+}
+
+getLinks();
