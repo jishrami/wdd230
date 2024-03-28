60f25067281706cfd84e140f901e941f0f64a20e
jishrami <jramirez@byui.edu>
2024-03-28 08:43:23 -0600

    added announcement banner that only opens on Mon-Wed

--- chamber/scripts/hamburgerButton.js
+++ chamber/scripts/hamburgerButton.js
@@ -1,7 +1,25 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('#navigation');
+const bannerClose = document.querySelector("#close");
+const banner = document.querySelector("#banner");
+const day = new Date();
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+bannerClose.addEventListener('click', () => {
+	banner.style.display = "none";
+})
+
+function closedBanner() {
+	if (day.getDay() === 1 || day.getDay() === 2 || day.getDay() === 3) {
+		banner.style.display = "block";
+	}
+	else {
+		banner.style.display = "none";
+	}
+}
+
+closedBanner()
